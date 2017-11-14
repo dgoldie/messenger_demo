00710706c8959bfe6e290bb3f2de24bdfaf10650
Doug Goldie <dgoldie15@gmail.com>
2017-11-14 13:06:01 -0800

    finish listen demo

--- app/bot/listen.rb
+++ app/bot/listen.rb
@@ -9,6 +9,21 @@ Facebook::Messenger::Subscriptions.subscribe(
   :access_token => ENV["ACCESS_TOKEN"]
 )
 
+# Bot.on :message do |message|
+#   Rails.logger.info "got your message! - #{message.inspect}"
+# end
+
+# message.id          # => 'mid.1457764197618:41d102a3e1ae206a38'
+# message.sender      # => { 'id' => '1008372609250235' }
+# message.sent_at     # => 2016-04-22 21:30:36 +0200
+# message.text        # => 'Hello, bot!'
+#
 Bot.on :message do |message|
-  Rails.logger.info "got your message! - #{message.inspect}"
+  Bot.deliver(
+    {
+      :recipient => message.sender,
+      :message => { :text => message.text }
+    },
+    :access_token => ENV["ACCESS_TOKEN"]
+  )
 end
