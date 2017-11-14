2ecc5921885493e5183404d9c918d050921945b8
Doug Goldie <dgoldie15@gmail.com>
2017-11-14 12:03:47 -0800

    fix logger

--- app/bot/listen.rb
+++ app/bot/listen.rb
@@ -3,12 +3,12 @@
 require "facebook/messenger"
 include Facebook::Messenger
 
-Logger.info "listen: access token is '#{ENV['ACCESS_TOKEN'].inspect}'"
+Rails.logger.info "listen: access token is '#{ENV['ACCESS_TOKEN'].inspect}'"
 
 Facebook::Messenger::Subscriptions.subscribe(
   :access_token => ENV["ACCESS_TOKEN"]
 )
 
 Bot.on :message do |message|
-  Logger.info "got your message! - #{message.inspect}"
+  Rails.logger.info "got your message! - #{message.inspect}"
 end
